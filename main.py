b0e203e68d34d91fd342c8a91dca387fb9309ea6
bruhseriously <s10240010@connect.np.edu.sg>
2022-08-01 14:59:10 +0800

    edited mainpy

---
+++ main.py
@@ -0,0 +1,9 @@
+import api, cash_on_hand, overheads, profit_loss
+
+def main():
+    forex = api.api_function()
+    overheads.overhead_function(forex)
+    cash_on_hand.cashonhand_function(forex)
+    profit_loss.profitloss_function(forex)
+
+main()
