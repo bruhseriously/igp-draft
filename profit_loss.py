f591042a2134297878f38b0663f111ff6e0acf5b
S10241940@connect.np.edu.sg <s10241940@connect.np.edu.sg>
2022-08-01 17:02:51 +0800

    marcelle has completed profit_loss.py

---
+++ profit_loss.py
@@ -0,0 +1,28 @@
+import csv
+from pathlib import Path
+
+file_path = Path.cwd()/'summary_report.txt'
+pl_csv = Path.cwd()/'csv_reports'/'Profit and Loss.csv'
+
+def profitloss_function(forex):
+    if pl_csv.exists():
+        with pl_csv.open(mode = 'r', encoding = 'UTF-8', errors = 'ignore') as csvfile:
+            reader = csv.reader(csvfile)
+            day = []
+            netprofit = []
+            next(reader)
+            for row in reader:
+                day.append(float(row[0]))
+                netprofit.append(float(row[4]) * forex)
+    count = 0
+    for amount in range(len(netprofit) - 1):
+        diff = netprofit[amount] - netprofit[amount + 1]
+        if diff > 0:
+            if file_path.exists():
+                with file_path.open(mode = 'a', encoding = 'UTF-8', errors = 'ignore') as file:
+                    text = file.write(f'\n[PROFIT DEFICIT] DAY: {day[amount + 1]}, AMOUNT: SGD{diff:.2f}')
+            count += 1
+    if count == 0:
+        if file_path.exists():
+            with file_path.open(mode = 'a', encoding = 'UTF-8', errors = 'ignore') as file:
+                text = file.write(f'\n[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
