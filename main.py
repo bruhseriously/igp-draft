d8ef5af456a13010ed41d4f256581a7323dd6be9
bruhseriously <s10240010@connect.np.edu.sg>
2022-08-03 01:49:15 +0800

    edited codes + clean up

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import api, cash_on_hand, overheads, profit_loss
+from pathlib import Path
+
+file_path = Path.cwd()/'summary_report.txt'
+file_path.touch()
 
 def main():
     forex = api.api_function()
